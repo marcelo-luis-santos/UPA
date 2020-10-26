a2d98095f53634f4acceda245a1aa51ac974ee81
Marcelo Santos <marcelo@connectedinteractive.com>
2020-10-26 16:35:05 -0700

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// namespace Game
+// {
+    public class GameManager
+    {
+        public string ReturnValue()
+        {
+            return "TestValue";
+        }
+    }
+// }
---
+++ Assets/Tests/GameManagerTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+// using Game;
+
+namespace Tests
+{
+    public class GameManagerTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void GameManagerTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator GameManagerTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+        [Test]
+        public void TestReturnValue()
+        {
+            GameManager gameManager = new GameManager();
+            Assert.AreEqual("TestValue", gameManager.ReturnValue());
+        }
+
+        [Test]
+        public void TestReturnValueFail()
+        {
+            GameManager gameManager = new GameManager();
+            Assert.AreEqual("TestValue1", gameManager.ReturnValue());
+        }
+    }
+}
